99778bd102b4b64281d3b1de54b5f4a97d0ad9bc
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2025-05-04 22:54:17 -0600

    add routing to HomePage and AboutPage in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes} from "react-router"
+import HomePage from "./pages/HomePage"
+import AboutPage from "./pages/AboutPage"
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </div>
   )
 }
 
