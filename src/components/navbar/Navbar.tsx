6c87461f0bd8bf373613bd61029c8842c8cee005
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2025-05-05 23:45:27 -0600

    update Navbar component styles for improved layout and responsiveness

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,10 +2,10 @@ import {ButtonDarkMode} from "@/components/navbar/ButtonDarkMode"
 
 function Navbar() {
   return (
-    <nav>
-        <h1>App</h1>
-        <div>
-            <ul>
+    <nav className="flex justify-between px-5 py-3">
+        <h1 className="text-2xl font-semibold">App</h1>
+        <div className="flex items-center gap-5">
+            <ul className="flex gap-5">
                 <li>Home</li>
                 <li>About</li>
             </ul>
