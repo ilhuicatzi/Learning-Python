f89cf9db032604427919cd182c893bbe2ebd4c14
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2025-05-05 23:58:40 -0600

    update HomePage component to include introductory blog content about learning Python

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,22 @@
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <section className="">
+      <article>
+        <h1 className="text-3xl font-bold">Blog: Aprendiendo Python </h1>
+        <p>
+          Este blog está dedicado a la enseñanza de Python, un lenguaje de programación versátil y poderoso. Aquí encontrarás tutoriales, ejemplos y recursos para aprender Python desde cero o mejorar tus habilidades existentes.
+        </p>
+      </article>
+      <article>
+        <h2 className="text-2xl font-bold">¿Por qué aprender Python?</h2>
+        <p>
+          Python es un lenguaje de programación fácil de aprender y usar. Es ideal para principiantes y se utiliza en una amplia variedad de aplicaciones, desde desarrollo web hasta análisis de datos y aprendizaje automático.
+        </p>
+      </article>
+    </section>
+    </div>
   )
 }
 
